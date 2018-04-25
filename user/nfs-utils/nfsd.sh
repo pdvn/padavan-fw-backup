ab24bec83f2b3cca61c613d485381a0feb4a0e83
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-04-25 12:19:33 +0300

    firmware: use rpcbind instead of portmap for nfs

--- user/nfs-utils/nfsd.sh
+++ user/nfs-utils/nfsd.sh
@@ -2,45 +2,45 @@
 
 func_start()
 {
-	# check portmap (needed for NFS)
-	if [ -z "`pidof portmap`" ] ; then
-		/sbin/portmap
+	# check rpcbind (needed for NFS)
+	if [ -z "`pidof rpcbind`" ] ; then
+		/sbin/rpcbind
 		sleep 1
 	fi
-	
+
 	# check NFS server already running
 	if [ -n "`pidof nfsd`" ] && [ -n "`pidof rpc.mountd`" ] ; then
 		# reload exports only
 		[ -f /etc/exports ] && /sbin/exportfs -r
 		return 0
 	fi
-	
+
 	touch /etc/exports
-	
+
 	mkdir -p /var/lib/nfs/sm
 	mkdir -p /var/lib/nfs/sm.bak
 	mkdir -p /var/lib/nfs/rpc_pipefs
-	
+
 	echo -n > /var/lib/nfs/etab
 	echo -n > /var/lib/nfs/rmtab
 	echo -n > /var/lib/nfs/xtab
-	
+
 	if [ -z "`cat /proc/filesystems | grep nfsd`" ] ; then
 		modprobe -q nfsd
 		mount -t nfsd nfsd /proc/fs/nfsd
 	fi
-	
+
 	if ! grep -q /var/lib/nfs/rpc_pipefs /proc/mounts ; then
 		mount -t rpc_pipefs rpc_pipefs /var/lib/nfs/rpc_pipefs
 	fi
-	
+
 	echo 65536 > /proc/fs/nfsd/max_block_size
-	
+
 	/sbin/exportfs -r
-	
+
 	/sbin/rpc.nfsd -N4 -N4.1 1
 	/sbin/rpc.mountd
-	
+
 	if [ $? -eq 0 ] ; then
 		logger -t "NFS server" "daemon is started"
 	fi
@@ -48,7 +48,7 @@ func_start()
 
 func_reload()
 {
-	if [ -n "`pidof portmap`" ] && [ -f /etc/exports ] ; then
+	if [ -n "`pidof rpcbind`" ] && [ -f /etc/exports ] ; then
 		/sbin/exportfs -r
 	fi
 }
@@ -57,23 +57,23 @@ func_stop()
 {
 	killall -q rpc.mountd
 	killall -q -2 nfsd
-	
+
 	for i in 1 2 3 ; do
 		[ -z "`pidof nfsd`" ] && break
 		sleep 1
 	done
-	
+
 	[ -n "`pidof nfsd`" ] && killall -q -9 nfsd
-	
-	if [ -n "`pidof portmap`" ] ; then
+
+	if [ -n "`pidof rpcbind`" ] ; then
 		if [ -f /var/lib/nfs/etab ] ; then
 			/sbin/exportfs -ua
 		fi
 	fi
-	
+
 	umount /var/lib/nfs/rpc_pipefs 2>/dev/null
 	umount /proc/fs/nfsd 2>/dev/null
-	
+
 	rm -f /var/lib/nfs/etab
 }
 
